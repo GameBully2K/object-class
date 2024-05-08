b3aef1fb0a8146d58d61681251335e53507050f4
Bilal Mbarki <bilal.ftb2000@gmail.com>
2024-05-08 06:10:15 +0100

    working ai api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,12 @@
 	import axios from 'axios';
 
 	let uploadedImage;
-	let image;
+	let blob;
 	let promise;
+	let results;
 
-    const accountID = import.meta.env.CLOUDFLARE_ACCOUNT_ID;
-    const token = import.meta.env.CLOUDFLARE_TOKEN;
+	const accountID = import.meta.env.CLOUDFLARE_ACCOUNT_ID;
+	const token = import.meta.env.CLOUDFLARE_TOKEN;
 
 	async function handleFileChange(event) {
 		const file = event.target.files[0];
@@ -15,36 +16,28 @@
 
 		reader.onload = async () => {
 			uploadedImage = reader.result;
+			blob = await fetch(uploadedImage).then((res) => res.blob());
 		};
 		reader.readAsDataURL(file);
-		image = await fetch(uploadedImage).then((res) => res.blob());
-		promise = await sendToCloudflare(image);
-        console.log(promise);
+
+		console.log('here is ' + JSON.stringify(blob));
+		//blob = await image.blob();
+		//promise = await sendToCloudflare(image);
 	}
 
-	async function handleURLChange() {
-		image = await fetch(uploadedImage).then((res) => res.blob());
-		promise = await sendToCloudflare(image);
+	async function handleURLChange(event) {
+		uploadedImage = event.target.value;
+		blob = await fetch(uploadedImage).then((res) => res.blob());
+		//promise = await sendToCloudflare(image);
 	}
 
-	async function sendToCloudflare(blob) {
-		const options = {
-			method: 'POST',
-			url: 'https://api.cloudflare.com/client/v4/accounts/'+accountID+'/ai/run/@cf/facebook/detr-resnet-50',
-			headers: {
-				'Content-Type': 'application/octet-stream',
-				Authorization: 'Bearer '+token
-			},
-			data: blob
-		};
+	// async function sendToCloudflare(blob) {
+	// 	let res = await axios.post('/cloudflare', blob,{headers:{'Content-Type': 'image/*'}});
+	// 	res = res.data;
 
-		try {
-			const response = await axios.request(options);
-			console.log(response.data);
-		} catch (error) {
-			console.error(error);
-		}
-	}
+	// 	console.log(res);
+	// 	results = res;
+	// }
 </script>
 
 <div class="flex gap-6 p-20">
@@ -54,15 +47,13 @@
 		<h3 class="h3 font-semibold">Upload Picture</h3>
 		<FileDropzone name="files" accept="image/*" class="h-[50%]" on:change={handleFileChange} />
 		<label class="label">
-			<span class="h3 font-semibold">Input</span>
-			<form on:submit|preventDefault={handleURLChange}>
-				<input
-					class="input"
-					type="url"
-					placeholder="https://examples.com/image.png"
-					bind:value={uploadedImage}
-				/>
-			</form>
+			<span class="h3 font-semibold">URL</span>
+			<input
+				class="input"
+				type="url"
+				placeholder="https://examples.com/image.png"
+				on:change={handleURLChange}
+			/>
 		</label>
 	</div>
 
@@ -71,61 +62,49 @@
 	>
 		{#if uploadedImage}
 			<div class="justify-left flex w-[20%] w-full items-start">
-				<img alt class="h-auto w-[50%] rounded-lg" src={uploadedImage} />
-				<div class="flex h-full w-[60%] flex-col items-center justify-center gap-4">
+				<img alt class="h-auto w-[50%] h-[50vh] object-cover rounded-3xl" src={uploadedImage} />
+				<div class="flex h-full w-[50%] flex-col items-center justify-center gap-4">
 					<h3 class="h2 font-semibold">Results</h3>
-                    <!-- {#await promise}
-						<div class="flex flex-col items-center justify-center">
-							<div class="placeholder-circle w-16" />
-							<div class="placeholder" />
-						</div>
-					{:then response}
-						<nav class="list-nav">
-							<ul>
-								<div
-									class="flex snap-x snap-mandatory scroll-px-4 gap-4 overflow-x-auto scroll-smooth px-4 py-10"
-								>
-									{#each response.result as item}
-										<li>
-											<a href="/elements/lists">
-												<span class="badge bg-primary-500"
-													>{Math.round(item.score * 100) == 100
-														? 99
-														: Math.round(item.score * 100)}%</span
-												>
-												<span class="h3 flex-auto text-start font-semibold">{item.label}</span>
-											</a>
-										</li>
-									{/each}
-								</div>
-							</ul>
-						</nav>
-					{:catch error}
-						<div class="alert-error alert">
-							<p>{error.message}</p>
-						</div>
-					{/await} -->
-                    
-                    <nav class="list-nav">
-                        <ul>
-                            <div
-                                class="flex flex-col snap-x snap-mandatory scroll-px-4 gap-4 overflow-x-auto scroll-smooth px-4 py-10"
-                            >
-                                {#each [{color:"red", score:0.9990994930267334}, {color:"green", score:0.785994930267334} ] as item}   
-                                    <li>
-                                        <a href="/elements/lists">
-                                            <span class="badge bg-primary-500"
-                                                >{Math.round(item.score * 100) == 100
-                                                    ? 99
-                                                    : Math.round(item.score * 100)}%</span
-                                            >
-                                            <span class="h3 flex-auto text-start font-semibold">{item.label}</span>
-                                        </a>
-                                    </li>
-                                {/each}
-                            </div>
-                        </ul>
-                    </nav>
+					{#if blob}
+						{#await axios.post('/cloudflare', blob, { headers: { 'Content-Type': 'image/*' } })}
+							<div class="flex flex-row gap-4 items-center justify-center">
+								<div class="placeholder-circle w-10 animate-pulse" />
+								<div class="placeholder w-20 animate-pulse" />
+							</div>
+						{:then response}
+							<nav class="list-nav">
+								<ul>
+									<div
+										class="flex flex-col snap-x snap-mandatory scroll-px-4 gap-4 overflow-x-auto scroll-smooth px-4 py-10"
+									>
+										{#if response.data.length != 0}
+											{#each response.data as item}
+												<li>
+													<a href="/elements/lists">
+														<span class="badge bg-primary-500"
+															>{Math.round(item.score * 100) == 100
+																? 99
+																: Math.round(item.score * 100)}%</span
+														>
+														<span class="h3 flex-auto text-start font-semibold">{item.label}</span>
+													</a>
+												</li>
+											{/each}
+										{:else}
+											<li>
+												<span class="badge bg-primary-500">100%</span>
+												<span class="h3 flex-auto text-start font-semibold">No results found</span>
+											</li>
+										{/if}
+									</div>
+								</ul>
+							</nav>
+						{:catch error}
+							<div class="alert-error alert">
+								<p>{error.message}</p>
+							</div>
+						{/await}
+					{/if}
 				</div>
 			</div>
 		{:else}
---
+++ src/routes/cloudflare/+server.js
@@ -0,0 +1,48 @@
+import { json } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
+import { CLOUDFLAREAPIKEY, CLOUDFLAREID } from '$env/static/private';
+
+const accountID = CLOUDFLAREID;
+const token = CLOUDFLAREAPIKEY;
+
+console.log(accountID, token);
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({request}) {
+  const url = `https://api.cloudflare.com/client/v4/accounts/${accountID}/ai/run/@cf/facebook/detr-resnet-50`;
+  const body = await request.blob();
+  console.log(body);
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'image/*', // Forward content type from the request
+      },
+      body: body,
+    });
+
+    let res = await response.json();
+    res = res.result;
+    let tempLabel = [];
+    let tobedeleted = [];
+    for (let i = 0; i < res.length; i++) {
+        if (res[i].score > 0.3) {
+            if (tempLabel.includes(res[i].label)) {
+                tobedeleted.push(i);
+            } else {
+                tempLabel.push(res[i].label);
+            }
+        } else {
+            tobedeleted.push(i);
+        }
+    }
+    for (let i = tobedeleted.length - 1; i >= 0; i--) {
+        res.splice(tobedeleted[i], 1);
+    }
+
+    return json(res);
+  } catch (err) {
+    return error(500, err.message)
+  }
+}
