e5434a8fa487e59ee185f3f9b7d9542cb4c8c9ad
Bilal Mbarki <bilal.ftb2000@gmail.com>
2024-05-11 04:05:06 +0100

    changed color to match weblion's theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
     import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
-<div class="announcement-bar bg-primary-500 text-white py-2 px-4 flex items-center justify-between">
+<div class="announcement-bar bg-[#335C4C] text-white py-2 px-4 flex items-center justify-between">
     <div class="flex items-center">
         <svg class="h-5 w-5 mr-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0zM9 5a1 1 0 0 1 2 0v3a1 1 0 1 1-2 0V5zm0 7a1 1 0 0 1 2 0v1a1 1 0 0 1-2 0v-1z" clip-rule="evenodd"/>
